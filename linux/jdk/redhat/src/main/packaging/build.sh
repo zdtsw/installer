f6d813985ba1b35a5653af110fd81d95d44dc499
Wen Zhou <wenzhou@redhat.com>
2022-05-19 16:00:36 +0200

    feat: add capability to run installer per jdk version or distro
    	- do not support ARCH with "all" but select each arch
    	- remove support VERSION with "all"
    	  - in reality, above 2 cases rarely happen
    	- make DISTRO to only "all" "Debian" and "RPM"
    	  - RPM includes Suse and RedHat both
    	- pass in jdkversion as variable for rpm
    	- change from matrix to three different stages per each distro

--- linux/jdk/redhat/src/main/packaging/build.sh
+++ linux/jdk/redhat/src/main/packaging/build.sh
@@ -4,14 +4,18 @@ set -euxo pipefail
 # Ensure necessary directories for rpmbuild operation are present.
 rpmdev-setuptree
 
-# Build all spec files we can find
-targets="x86_64 ppc64le s390x aarch64 armv7hl"
+# Build specified target or build all (not s390x on jdk8)
+if ${buildArch} != "all"; then
+	targets=${buildArch}
+elif ${buildVersion} != "8"; then
+  	targets="x86_64 ppc64le aarch64 armv7hl s390x"
+else
+	targets="x86_64 ppc64le aarch64 armv7hl"	
+fi
+
 for spec in "$(ls /home/builder/build/generated/packaging/*.spec)"; do
 	spectool -g -R "$spec";
 	rpmbuild --nodeps -bs "$spec";
-	if [[ "$spec" =~ "temurin-8-jdk" ]]; then
-		targets="x86_64 ppc64le aarch64 armv7hl"
-	fi
 	for target in $targets; do
 		rpmbuild --target "$target" --rebuild /home/builder/rpmbuild/SRPMS/*.src.rpm;
 	done;
